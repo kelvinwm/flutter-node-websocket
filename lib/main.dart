4b7aa3539a347c90967d245bbd79995f7577a71f
Kelvin Muinde <kelvin@Kelvins-MacBook-Pro.local>
2019-08-01 22:43:30 +0300

    Graphql

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,7 @@
 import 'dart:io';
 import 'package:flutter/material.dart';
 import 'package:graphql_flutter/graphql_flutter.dart';
-import 'package:firebase_database/firebase_database.dart';
-
+//import 'package:firebase_database/firebase_database.dart';
 
 void main() {
   runApp(MaterialApp(
@@ -14,7 +13,8 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final HttpLink httpLink = HttpLink(
-      uri: 'http://10.0.2.2:5000/graphql',
+//      uri: 'http://10.10.154.39:5000/graphql',// Get Laptop Ip address
+      uri: 'https://knhsystem.herokuapp.com/graphql', // Get Laptop Ip address
     );
 
     ValueNotifier<GraphQLClient> client = ValueNotifier(
@@ -42,8 +42,13 @@ class AbsorbHere extends StatefulWidget {
 class _AbsorbHereState extends State<AbsorbHere> {
   String queryAuthors = r"""
                         query  GetAuthors{
-                          author{
-                            patientid
+                           allusers {
+                            email
+                            hospitalName
+                            hospitalPin
+                            role
+                            userFrom
+                            password
                           }
                       }
                       """;
@@ -51,84 +56,40 @@ class _AbsorbHereState extends State<AbsorbHere> {
   @override
   Widget build(BuildContext context) {
     //database referene.
-    var recentJobsRef = FirebaseDatabase.instance
-        .reference()
-        .child('recent')
-        .orderByChild('created_at') //order by creation time.
-        .limitToFirst(10);
     return Scaffold(
       appBar: AppBar(
         title: Text("THROGUH AT YOU"),
       ),
-      body: StreamBuilder(
-        stream: recentJobsRef.onValue,
-        builder: (context, snap) {
-          if (snap.hasData && !snap.hasError &&
-              snap.data.snapshot.value != null) {
-            DataSnapshot snapshot = snap.data.snapshot;
-            List item = [];
-            List _list = [];
-//it gives all the documents in this list.
-            _list = snapshot.value;
-//Now we're just checking if document is not null then add it to another list called "item".
-//I faced this problem it works fine without null check until you remove a document and then your stream reads data including the removed one with a null value(if you have some better approach let me know).
-            _list.forEach((f) {
-              if (f != null) {
-                item.add(f);
-              }
-            }
-            );
-            return snap.data.snapshot.value == null
-//return sizedbox if there's nothing in database.
-                ? SizedBox()
-//otherwise return a list of widgets.
-                : ListView.builder(
-              scrollDirection: Axis.horizontal,
-              itemCount: item.length,
-              itemBuilder: (context, index) {
-                return ListTile(
-                    title: Text("kenya")
-//                return _containerForRecentJobs(
-//                    item[index]['title']
-                );
-              },
-            );
-          } else {
-            return Center(child: CircularProgressIndicator());
+      body: Query(
+        options: QueryOptions(document: queryAuthors),
+        // Just like in apollo refetch() could be used to manually trigger a refetch
+        builder: (QueryResult result, {VoidCallback refetch}) {
+          if (result.loading) {
+            return CircularProgressIndicator();
           }
+          if (result.errors != null) {
+            return Text(result.errors.toString());
+          }
+          if (result.data == null) {
+            return Center(child: Text("No Data Found !"));
+          }
+
+          // it can be either Map or List
+//          List repositories = result.data['patientid'];
+
+          debugPrint(result.data["allusers"].length.toString());
+          return ListView.builder(
+            itemBuilder: (BuildContext context, int index) {
+              if (result.data['allusers'][index]['email'] == null) {
+                result.data['allusers'][index]['email'] = '';
+              }
+              return ListTile(
+                  title: Text(result.data['allusers'][index]['email']));
+            },
+            itemCount: result.data['allusers'].length,
+          );
         },
       ),
-
-
-//      Query(
-//        options: QueryOptions(document: queryAuthors),
-//        // Just like in apollo refetch() could be used to manually trigger a refetch
-//        builder: (QueryResult result, {VoidCallback refetch}) {
-//          if (result.loading) {
-//            return CircularProgressIndicator();
-//          }
-//          if (result.errors != null) {
-//            return Text(result.errors.toString());
-//          }
-//          if (result.data == null) {
-//            return Center(child: Text("No Data Found !"));
-//          }
-//
-//          // it can be either Map or List
-////          List repositories = result.data['patientid'];
-//
-//          debugPrint(result.data["author"].length.toString());
-//          return ListView.builder(
-//            itemBuilder: (BuildContext context, int index) {
-//              return ListTile(
-//                  title: Text(result.data['author'][index]['patientid']));
-//            },
-//            itemCount: result.data['author'].length,
-//          );
-//        },
-//      ),
     );
   }
 }
-
-
