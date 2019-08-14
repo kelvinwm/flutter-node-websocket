a53c085e31194b8015d743fe0897d2e70bf654dc
Kelvin Muinde <kelvin@Kelvins-MacBook-Pro.local>
2019-08-14 16:34:45 +0300

    various message app backup

--- lib/main.dart
+++ lib/main.dart
@@ -1,95 +1,159 @@
-import 'dart:io';
+import 'package:flutter/foundation.dart';
+import 'package:web_socket_channel/io.dart';
 import 'package:flutter/material.dart';
-import 'package:graphql_flutter/graphql_flutter.dart';
-//import 'package:firebase_database/firebase_database.dart';
+import 'dart:convert';
+import 'package:web_socket_channel/web_socket_channel.dart';
 
-void main() {
-  runApp(MaterialApp(
-    home: MyApp(),
-  ));
-}
+void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    final HttpLink httpLink = HttpLink(
-//      uri: 'http://10.10.154.39:5000/graphql',// Get Laptop Ip address
-      uri: 'https://knhsystem.herokuapp.com/graphql', // Get Laptop Ip address
-    );
-
-    ValueNotifier<GraphQLClient> client = ValueNotifier(
-      GraphQLClient(
-        cache: NormalizedInMemoryCache(
-          dataIdFromObject: typenameDataIdFromObject,
-        ),
-        link: httpLink as Link,
-      ),
-    );
-    return GraphQLProvider(
-      client: client,
-      child: CacheProvider(
-        child: AbsorbHere(),
+    final title = 'WebSocket Demo';
+    return MaterialApp(
+      title: title,
+      home: MyHomePage(
+        title: title,
+//        channel: IOWebSocketChannel.connect('ws://192.168.5.11:3030'),
+//        channel:
+//            IOWebSocketChannel.connect('ws://192.168.5.11:3030/echo1/James'),
+//        channel: IOWebSocketChannel.connect('ws://echo.websocket.org'),
       ),
     );
   }
 }
 
-class AbsorbHere extends StatefulWidget {
+class MyHomePage extends StatefulWidget {
+  final String title;
+  final WebSocketChannel channel;
+
+  MyHomePage({Key key, @required this.title, @required this.channel})
+      : super(key: key);
+
   @override
-  _AbsorbHereState createState() => _AbsorbHereState();
+  _MyHomePageState createState() => _MyHomePageState();
 }
 
-class _AbsorbHereState extends State<AbsorbHere> {
-  String queryAuthors = r"""
-                        query  GetAuthors{
-                           allusers {
-                            email
-                            hospitalName
-                            hospitalPin
-                            role
-                            userFrom
-                            password
-                          }
-                      }
-                      """;
+class _MyHomePageState extends State<MyHomePage> {
+  TextEditingController _controller = TextEditingController();
+  WebSocketChannel channel;
+  TextEditingController controller;
+  final List<String> list = [];
 
+  @override
+  void initState() {
+    super.initState();
+    channel = IOWebSocketChannel.connect('ws://192.168.5.11:3030/echo1/James');
+    controller = TextEditingController();
+//    try {
+//      channel.stream.listen((data) {
+//        // handling of the incoming messages
+//        debugPrint(data);
+//        setState(() {
+//          list.add(data);
+//        });
+//      }, onError: (error, StackTrace stackTrace) {
+//        // error handling
+//      }, onDone: () {
+//        // communication has been closed
+//        debugPrint("socket was closed");
+//        reconnect();
+//      });
+//    } catch (e) {
+//      /// TODO handle connection failure
+//    }
+  }
+
+  reconnect() {
+    Future.delayed(const Duration(milliseconds: 10000), () {
+      setState(() {
+        // Here you can write your code for open new view
+        channel =
+            IOWebSocketChannel.connect('ws://192.168.5.11:3030/echo1/James');
+//        try {
+//          channel.stream.listen((data) {
+//            // handling of the incoming messages
+//            debugPrint(data);
+//            setState(() {
+//              list.add(data);
+//            });
+//          }, onError: (error, StackTrace stackTrace) {
+//            // error handling
+//          }, onDone: () {
+//            // communication has been closed
+//            debugPrint("socket was closed");
+//            reconnect();
+//          });
+//        } catch (e) {
+//          /// TODO handle connection failure
+//        }
+      });
+    });
+
+    debugPrint("Socket closed. Trying to reconnect");
+  }
+
+//  var jsonString = json.encode({'kenya':'TukoPamoja', 'Id':'Huduma Number'});
   @override
   Widget build(BuildContext context) {
-    //database referene.
     return Scaffold(
       appBar: AppBar(
-        title: Text("THROGUH AT YOU"),
+        title: Text(widget.title),
       ),
-      body: Query(
-        options: QueryOptions(document: queryAuthors),
-        // Just like in apollo refetch() could be used to manually trigger a refetch
-        builder: (QueryResult result, {VoidCallback refetch}) {
-          if (result.loading) {
-            return CircularProgressIndicator();
-          }
-          if (result.errors != null) {
-            return Text(result.errors.toString());
-          }
-          if (result.data == null) {
-            return Center(child: Text("No Data Found !"));
-          }
-
-          // it can be either Map or List
-//          List repositories = result.data['patientid'];
-
-          debugPrint(result.data["allusers"].length.toString());
-          return ListView.builder(
-            itemBuilder: (BuildContext context, int index) {
-              if (result.data['allusers'][index]['email'] == null) {
-                result.data['allusers'][index]['email'] = '';
-              }
-              return ListTile(
-                  title: Text(result.data['allusers'][index]['email']));
-            },
-            itemCount: result.data['allusers'].length,
-          );
-        },
+      body: Padding(
+        padding: const EdgeInsets.all(20.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            Form(
+              child: TextFormField(
+                controller: _controller,
+                decoration: InputDecoration(labelText: 'Send a message'),
+              ),
+            ),
+//            Column(
+//              children: list.map((data) => Text(data)).toList(),
+//            )
+            StreamBuilder(
+              stream: channel.stream,
+              builder: (context, snapshot) {
+                print("connection state ${snapshot.connectionState}");
+                if (snapshot.connectionState
+                    .toString()
+                    .contains("ConnectionState.done")) {
+                  reconnect();
+                }
+                debugPrint('VIPI ${snapshot.data}');
+                return Padding(
+                  padding: const EdgeInsets.symmetric(vertical: 24.0),
+                  child: Text(snapshot.hasData ? '${snapshot.data}' : ''),
+                );
+              },
+            )
+          ],
+        ),
       ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _sendMessage,
+        tooltip: 'Send message',
+        child: Icon(Icons.send),
+      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
+
+  void _sendMessage() {
+    if (_controller.text.isNotEmpty) {
+      channel.sink.add(_controller.text);
+//      widget.channel.sink.add(jsonString);
+    }
+  }
+
+  @override
+  void dispose() {
+    channel.sink.close();
+    debugPrint("The socket closed meeen");
+    super.dispose();
+  }
 }
+
+//["James","ethbrtojgert"]
